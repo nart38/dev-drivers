f083c1ff8c09d181cf1f4b0440b63d42cb118b3c
nart38 <sezer_1966_sezgin@hotmail.com>
2024-01-24 19:55:14 +0300

    we are so git initialized

---
+++ freebsd/hello.c
@@ -0,0 +1,25 @@
+#include <sys/conf.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/types.h>
+
+static int hello_load(module_t mod, int cmd, void *arg) {
+  int err = 0;
+  switch (cmd) {
+  case MOD_LOAD:
+    log(1, "Hello from FreeBSD module.");
+    break;
+  case MOD_UNLOAD:
+    log(1, "Goodbye, Moonman!");
+    break;
+  default:
+    err = EOPNOTSUPP;
+    break;
+  }
+  return err;
+}
+
+static moduledata_t hello_mod = {"hello", hello_load, NULL};
+DECLARE_MODULE(hello, hello_mod, SI_SUB_KLD, SI_ORDER_ANY);
